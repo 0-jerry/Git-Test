1e35f5b43719f8717ec44d13631d67196f0849a0
이재영 <ldc1000@naver.com>
2024-12-22 09:00:07 +0900

    init

---
+++ GitTest/GitTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitTest
+//
+//  Created by t2023-m0072 on 12/22/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
